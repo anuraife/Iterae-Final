62cd3a823a6e0a528764d9cdf8ae17227eaaee3a
Anu Raife <anuradharaife2020@u.northwestern.edu>
2018-03-12 09:51:30 -0500

    still fixing

--- public/js/popup.js
+++ public/js/popup.js
@@ -6,18 +6,17 @@ function onLoad() {
   // console.log(data);
   var parsedURL = new URL(window.location.href);
   var level = parsedURL.searchParams.get("level");
-  console.log(level);
-  if (data.level == 0)
+  if (level == 0)
   {
       document.getElementById('chosenlevel').innerHTML = "Beginner";
       document.getElementById('chosenbpm').innerHTML = "100";
   }
- if (data.level==1)
+ if (level==1)
   {
     document.getElementById('chosenlevel').innerHTML = "Intermediate";
     document.getElementById('chosenbpm').innerHTML = "150";
   }
-  if (data.level==2)
+  if (level==2)
    {
      document.getElementById('chosenlevel').innerHTML = "Expert";
      document.getElementById('chosenbpm').innerHTML = "180";
